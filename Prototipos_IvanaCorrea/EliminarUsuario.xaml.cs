0fadd6bfe9ff6eea63e07cecd0ad2832d50a4b3d
Ivana Correa <luzita_ivanita@hotmail.com>
2021-05-24 00:25:18 -0500

    Agregar archivos de proyecto.

---
+++ Prototipos_IvanaCorrea/EliminarUsuario.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prototipos_IvanaCorrea
+{
+    /// <summary>
+    /// Lógica de interacción para EliminarUsuario.xaml
+    /// </summary>
+    public partial class EliminarUsuario : Window
+    {
+        public EliminarUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Cancelar_Click(object sender, RoutedEventArgs e)
+        {
+            MenuUsuarios menuUsuarios = new MenuUsuarios();
+            menuUsuarios.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Prototipos_IvanaCorrea/MenuUsuarios.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prototipos_IvanaCorrea
+{
+    /// <summary>
+    /// Lógica de interacción para MenuUsuarios.xaml
+    /// </summary>
+    public partial class MenuUsuarios : Window
+    {
+        public MenuUsuarios()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Btn_Registrar_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrarUsuario registrarUsuario = new RegistrarUsuario();
+            registrarUsuario.Show();
+            this.Close();
+        }
+
+        private void Btn_Consultar_Click(object sender, RoutedEventArgs e)
+        {
+            ConsultarUsuario consultarUsuario = new ConsultarUsuario();
+            consultarUsuario.Show();
+            this.Close();
+        }
+
+        private void Btn_Eliminar_Click(object sender, RoutedEventArgs e)
+        {
+            EliminarUsuario eliminarUsuario = new EliminarUsuario();
+            eliminarUsuario.Show();
+            this.Close();
+        }
+
+        private void Btn_Modificar_Click(object sender, RoutedEventArgs e)
+        {
+            ModificarUsuario modificarUsuario = new ModificarUsuario();
+            modificarUsuario.Show();
+            this.Close();
+        }
+
+        private void Btn_Regresar_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
